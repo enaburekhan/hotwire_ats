2d60803bd88098f4c1b6f59656ea5806803412c9
enaburekhan <ericenaburekhan2018@gmail.com>
2022-12-04 15:06:18 +0100

    Set up app

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  plugins: [require('@tailwindcss/forms')],
+};
